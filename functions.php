f4bb2bc60f25d7d1aefb76938eaa479621470996
Arbahud Rio Daroyni <roydotendless@gmail.com>
2021-01-19 23:18:32 +0700

    test

--- functions.php
+++ functions.php
@@ -102,49 +102,6 @@ function get_first_image_in_post() {
 	return $first_img;
 }
 
-// for redirect google amp page
-define( 'AMP_QUERY_VAR', apply_filters( 'amp_query_var', 'amp' ) );
-add_rewrite_endpoint( AMP_QUERY_VAR, EP_PERMALINK | EP_PAGES );
-function prefix_url_rewrite() {
-	if( get_query_var( AMP_QUERY_VAR, false ) !== false ) {
-		if ( is_page() ) {
-      add_filter( 'template_include', function() {
-        return get_template_directory() . '/index-amp.php';
-      });
-		}
-		if ( is_single() ) {
-      add_filter( 'template_include', function() {
-        return get_template_directory() . '/single-amp.php';
-      });
-		}
-	}
-}
-add_action( 'template_redirect', 'prefix_url_rewrite' );
-// /.for redirect google amp page
-
-function amp_filter($content){
-	$patterns = []; $patterns_non_amp = []; $replacements = []; $replacements_non_amp = [];
-
-	$patterns = [
-		'/<img loading="lazy" ([^>]+)?>/',
-		'/<iframe ([^>]+)? allow="([^>]+)?>(.*?)<\/iframe>/'
-	];
-	$replacements = [
-		'<amp-img $1 layout="responsive"></amp-img>',
-		'<amp-iframe $1 sandbox="allow-scripts allow-same-origin" layout="responsive"></amp-iframe>'
-	];
-
-	$patterns_non_amp = [ '/<img src="(.*?)" srcset="(.*?)" ([^>]+)?>/' ];
-	$replacements_non_amp = [ '<img src="'.get_default_image().'" alt="'.get_the_title().'" class="lazy image" data-id="" data-src="$1" data-srcset="$2" $3>' ];
-	
-	if (isset($_GET['amp'])) {
-		return preg_replace($patterns, $replacements, $content);
-	} else {
-		return preg_replace($patterns_non_amp, $replacements_non_amp, $content);
-	}
-}
-add_filter('the_content', 'amp_filter');
-
 // function get_id_attachment_by_url($url){
 // 	global $wpdb;
 // 	$attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $url )); 
@@ -158,3 +115,7 @@ add_filter('the_content', 'amp_filter');
 	
 // 	return $image = ($img == NULL) ? get_first_image_in_post() : $img;
 // }
+
+require_once( get_template_directory() . '/inc/wp-amp.php' );
+
+require_once( get_template_directory() . '/inc/structured-data.php' );
