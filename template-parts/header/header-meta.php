29bb9601c992d33e0b9c796aa5ff916bfd696f95
Arbahud Rio Daroyni <roydotendless@gmail.com>
2021-02-10 03:16:44 +0700

    update

--- template-parts/header/header-meta.php
+++ template-parts/header/header-meta.php
@@ -1,6 +1,6 @@
 <?= $amp = (isset($_GET['amp'])) ? '<script async src="https://cdn.ampproject.org/v0.js"></script>' : '' ?>
 <?= $amp_form = (isset($_GET['amp'])) ? '<script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>' : ''; ?>
-<?= $ads = (isset($_GET['amp'])) ? '<script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>' : '<script data-ad-client="ca-pub-5419652163962979" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' ?>
+<?= // $ads = (isset($_GET['amp'])) ? '<script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>' : '<script data-ad-client="ca-pub-5419652163962979" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' ?>
 
 <?php if (is_home() || is_front_page()){ ?>
 <title><?php bloginfo( 'description' ); ?> - <?php bloginfo( 'name' ); ?></title>
